1479b94abaf65fd2e1337d5a74c2b02869ac0b56
automateLinux <root@localhost>
2026-04-14 18:41:07 +0300

    #144: Add favicon to addnewfeature

---
+++ app/icon.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from 'next/og'
+
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 22,
+          background: '#2563eb',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: 6,
+          fontWeight: 700,
+        }}
+      >
+        A
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
